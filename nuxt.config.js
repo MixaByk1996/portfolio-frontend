8a8bcaa3831a8c11520e6440519afb6e96df82eb
MixaByk1996 <bykmikhail@gmail.com>
2023-11-26 23:58:00 +0300

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -47,14 +47,16 @@ export default {
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
-    baseURL: '/',
+    withCredentials:true,
+    baseURL: 'http://127.0.0.1:8000/api',
+
   },
 
   // Vuetify module configuration: https://go.nuxtjs.dev/config-vuetify
   vuetify: {
     customVariables: ['~/assets/variables.scss'],
     theme: {
-      dark: true,
+      dark: false,
       themes: {
         dark: {
           primary: colors.blue.darken2,
---
+++ pages/dashboard.vue
@@ -0,0 +1,60 @@
+<template>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app>
+      <template v-if="companies">
+        <v-list>
+          <v-subheader>Компании</v-subheader>
+
+            <v-list-group v-for="item in companies" :key="item.id" :value="item.name" >
+              <template v-slot:activator>
+                <v-list-item-content>
+                  <v-list-item-title v-text="item.name"></v-list-item-title>
+                </v-list-item-content>
+              </template>
+              <v-subheader>Проекты компани {{item.name}}</v-subheader>
+              <v-list-group v-for="project in item.projects" :key="project.id" class="pl-10">
+                <template v-slot:activator>
+                  <v-list-item-content>
+                    <v-list-item-title v-text="project.name"></v-list-item-title>
+                  </v-list-item-content>
+                </template>
+              </v-list-group>
+            </v-list-group>
+
+        </v-list>
+      </template>
+    </v-navigation-drawer>
+
+    <v-app-bar app>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+
+      <v-app-bar-title>Имя пользователя</v-app-bar-title>
+    </v-app-bar>
+    <v-footer color="indigo" app></v-footer>
+    <v-main>
+<!--      <v-btn>блаблабла</v-btn>-->
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import { ref } from 'vue'
+
+const drawer = ref(null)
+export default {
+  beforeMount() {
+    this.$axios.get('/company')
+      .then(response => {
+
+        this.companies = Array.from(response.data.data);
+        console.log(this.companies);
+      })
+  },
+  data() {
+    return {
+      drawer: null,
+      companies: null
+    }
+  }//() => ({ drawer: null }),
+}
+</script>
