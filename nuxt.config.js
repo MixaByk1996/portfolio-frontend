aeca8e060aeff72ab41b4cfcd8cd4637b812a75d
MixaByk1996 <bykmikhail@gmail.com>
2023-12-14 21:26:14 +0300

    changes

--- nuxt.config.js
+++ nuxt.config.js
@@ -48,7 +48,7 @@ export default {
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
     withCredentials:true,
-    baseURL: 'http://192.168.56.56/api',
+    baseURL: 'http://8657437b.com/api',
 
   },
 
